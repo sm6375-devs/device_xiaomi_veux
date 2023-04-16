b13ec9bb94477a687fae625ace1d55c15677ff93
Arian <arian.kulmer@web.de>
2023-04-16 17:32:17 +0200

    veux: Move to common sensors multihal
    Change-Id: Iba4df5ebb5d653cb2d3f68fce9e682d946d20935

--- device.mk
+++ device.mk
@@ -470,7 +470,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.xiaomi_holi-multihal \
+    android.hardware.sensors@2.1-service.xiaomi-multihal \
     libsensorndkbridge
 
 # Service Tracker
