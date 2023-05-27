1153caa3efc8bb13c8897e6fb4106c06fc9e755f
lect <96887074+lectfx@users.noreply.github.com>
2023-05-27 23:21:46 +0200

    veux: lineage bringup

--- lineage_veux.mk
+++ lineage_veux.mk
@@ -17,10 +17,10 @@ TARGET_SUPPORTS_QUICK_TAP := true
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-# Inherit some common PixelOS
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_veux
+PRODUCT_NAME := lineage_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
